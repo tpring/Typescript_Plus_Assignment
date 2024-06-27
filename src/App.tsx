a86903d140e8c3e57aad08cb42e369a9e9d232e5
tpring <pororo1@naver.com>
2024-06-27 09:50:22 +0900

    2024.06.27 fix: 이슈#1 ( tailwind CSS 사용, types 폴더명대신 models, 타입선언 분리,  handleToggle를 App.tsx에서 선언 )

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import CountryList from './components/CountryList';
 import { readCountry } from './api/Countries';
-import { Country } from './types/types';
+import { Country } from './models/types';
 
 function App() {
     const [countries, setCountries] = useState<Country[]>([]);
@@ -19,13 +19,24 @@ function App() {
         fetchData();
     }, []);
 
+    const handleToggle = (countrieName: string) => {
+        setCountries((prev) =>
+            prev.map((c) => {
+                if (c.name === countrieName) {
+                    return { ...c, favorite: !c.favorite };
+                }
+                return c;
+            })
+        );
+    };
+
     const favoriteCountries = countries.filter((country) => country.favorite);
     const unfavoriteCountries = countries.filter((country) => !country.favorite);
 
     return (
         <div>
-            <CountryList title="Favorite Countries" countries={favoriteCountries} setCountries={setCountries} />
-            <CountryList title="Countries" countries={unfavoriteCountries} setCountries={setCountries} />
+            <CountryList title="Favorite Countries" countries={favoriteCountries} onClick={handleToggle} />
+            <CountryList title="Countries" countries={unfavoriteCountries} onClick={handleToggle} />
         </div>
     );
 }
